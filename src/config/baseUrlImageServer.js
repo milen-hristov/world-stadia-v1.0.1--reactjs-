a958643afba0e772dea6daed0612b37aa7e8dc4d
Milen Hristov <milenskibg88@gmail.com>
2022-03-28 23:14:36 +0300

    Added cloudinary config

--- src/config/baseUrlImageServer.js
+++ src/config/baseUrlImageServer.js
@@ -1,9 +1,7 @@
-// const BASEURLIMAGEOPTIONS = {
-//     oldLocal: 'http://localhost:4000/api/user-profile',
-//     newLocal: 'http://localhost:8080',
-//     web: 'https://world-stadia-upload-img.herokuapp.com',
-// }
-
-// const baseUrlImageServer = BASEURLIMAGEOPTIONS.newLocal;
-
-// export default baseUrlImageServer;
+export const BASEURLIMAGEOPTIONS = {
+    // oldLocal: 'http://localhost:4000/api/user-profile',
+    // newLocal: 'http://localhost:8080',
+    // web: 'https://world-stadia-upload-img.herokuapp.com',
+    cloudinary: 'https://api.cloudinary.com/v1_1/milenski',
+    cloudinaryPreset: 'coeokruw',
+}
